faea3f4c1fee90a62413b478c123cf8a8008f4f0
Sruthii5 <sthukkani@hawk.iit.edu>
2021-03-18 16:09:14 -0500

    committed sort colors

---
+++ SortColors-75.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int temp = 0;
+        int current = 0;
+        while(current <= right){
+            if(nums[current] == 0 ){
+                temp = (nums[left]);
+                nums[left] = nums[current];
+                nums[current] = temp;
+                left++;
+                current++;
+                        
+            }
+            else if(nums[current]==2){
+                temp = nums[right];
+                nums[right]= nums[current];
+                nums[current] = temp;
+                right--;
+                
+                
+            }
+            else{current++;}
+        }
+        
+        
+    }
+}
